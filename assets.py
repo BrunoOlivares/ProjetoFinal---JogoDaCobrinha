ad519ff208c4812fba9614f0ae13cbd8a3d78478
Guilherme Paraiso <guilhermevp@al.insper.edu.br>
2021-11-29 09:51:23 -0300

    mecanica da frutinha

---
+++ assets.py
@@ -0,0 +1,3 @@
+def imagem_corpo(player,pedaco):
+    pedaco.image=player.image
+    return pedaco.image
--- fruta.py
+++ fruta.py
@@ -1,16 +1,3 @@
-def cria_fruta():
-    from os import device_encoding
-    import pygame
-    from pygame.sprite import Sprite
-    import random
-    contador_fruta = 0
-    tempo_atual = 0
-    while contador_fruta < 10:
-        tempo_atual = pygame.time.get_ticks()
-        
-        if tempo_atual > 5000:
-            fruta = comida(img_jog)
-            pedaços_da_cobra.add(fruta)
-            contador_fruta += 1
-            tempo_atual = 0
-
+import random
+x=random.randint(0,10)
+print(x)
